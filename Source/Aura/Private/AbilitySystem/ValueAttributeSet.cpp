753cfe8010d5dcf4b7dd1fe68c91d02f57d4bc85
Alicja Texler <emunah73@gmail.com>
2024-02-19 12:28:17 -0500

    add accessors

--- Source/Aura/Private/AbilitySystem/ValueAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/ValueAttributeSet.cpp
@@ -2,11 +2,14 @@
 
 
 #include "AbilitySystem/ValueAttributeSet.h"
-#include "AbilitySystem/ValueAbilitySystemComponent.h"
+
 #include <Net/UnrealNetwork.h>
 
 UValueAttributeSet::UValueAttributeSet() {
-
+  InitHealth(100.f);
+  InitMaxHealth(100.f);
+  InitMana(50.f);
+  InitMaxMana(50.f);
 
 }
 
