a28fa2d97f6e43b30e836a1248528a8b9646b190
Alicja Texler <emunah73@gmail.com>
2024-03-29 18:09:31 -0400

    added input tags and actions for abilities

---
+++ Source/Aura/Private/Input/ValueInputConfig.cpp
@@ -0,0 +1,22 @@
+// Copyright Philosophical Games
+
+#include "Input/ValueInputConfig.h"
+
+#include "InputAction.h"
+
+const UInputAction* UValueInputConfig::FindAbilityInputActionForTag(
+    const FGameplayTag& InputTag, bool bLogNotFound) const {
+  for (const auto& Action : AbilityInputActions) {
+    if (Action.InputAction && Action.GameplayTag.MatchesTagExact(InputTag)) {
+      return Action.InputAction;
+    }
+  }
+
+  if (bLogNotFound) {
+    UE_LOG(LogTemp, Error,
+           TEXT("Can't find AbilityInputAction for InputTag [%s], on "
+                "InputConfig [%s]"),
+           *InputTag.ToString(), *GetNameSafe(this));
+  }
+  return nullptr;
+}
---
+++ Source/Aura/Public/Input/ValueInputConfig.h
@@ -0,0 +1,37 @@
+// Copyright Philosophical Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "ValueInputConfig.generated.h"
+
+USTRUCT(BlueprintType)
+struct FValueInputAction {
+  GENERATED_BODY()
+
+  UPROPERTY(EditDefaultsOnly)
+  const class UInputAction* InputAction = nullptr;
+  UPROPERTY(EditDefaultsOnly)
+  FGameplayTag GameplayTag = FGameplayTag();
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UValueInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+  const UInputAction* FindAbilityInputActionForTag(
+     const FGameplayTag& InputTag, bool bLogNotFound = false) const;
+
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+  TArray<FValueInputAction> AbilityInputActions;
+	
+};
