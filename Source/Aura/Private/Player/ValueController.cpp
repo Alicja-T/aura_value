7a87a60f53e03edb20668a7223856304edca6cb4
Alicja Texler <emunah73@gmail.com>
2024-06-16 18:57:38 -0400

    using service in behavior tree

--- Source/Aura/Private/Player/ValueController.cpp
+++ Source/Aura/Private/Player/ValueController.cpp
@@ -148,8 +148,11 @@ void AValueController::AbilityInputTagReleased(FGameplayTag InputTag) {
         for (const FVector& PointLoc : NavPath->PathPoints) {
           Spline->AddSplinePoint(PointLoc, ESplineCoordinateSpace::World);
         }
-        CachedDestination = NavPath->PathPoints[NavPath->PathPoints.Num() - 1];
-        bAutoRunning = true;
+        if (NavPath->PathPoints.Num() > 0) {
+          CachedDestination =
+              NavPath->PathPoints[NavPath->PathPoints.Num() - 1];
+          bAutoRunning = true;
+        }
       }
     }
     FollowTime = 0.f;
