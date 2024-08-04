f4f979b27fbaf6fff22f7a50c41a80fecd21f762
Alicja Texler <emunah73@gmail.com>
2024-08-04 19:33:51 -0400

    slingshot animation

--- Source/Aura/Public/Character/ValueCharacterBase.h
+++ Source/Aura/Public/Character/ValueCharacterBase.h
@@ -45,7 +45,7 @@ class AURA_API AValueCharacterBase : public ACharacter,
   // Called when the game starts or when spawned
   virtual void BeginPlay() override;
 
-  UPROPERTY(EditAnywhere, Category = "Combat")
+  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
   TObjectPtr<USkeletalMeshComponent> Weapon;
   UPROPERTY(EditAnywhere, Category = "Combat")
   FName WeaponTipSocketName;
