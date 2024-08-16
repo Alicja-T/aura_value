ebf059e9e196a54ce1b099cf0668e7b7c858d45f
Alicja Texler <emunah73@gmail.com>
2024-08-15 20:34:02 -0400

    summoning locations

---
+++ Source/Aura/Private/AbilitySystem/Abilities/ValueSummonAbility.cpp
@@ -0,0 +1,33 @@
+// Copyright Philosophical Games
+
+
+#include "AbilitySystem/Abilities/ValueSummonAbility.h"
+#include <Kismet/KismetSystemLibrary.h>
+
+TArray<FVector> UValueSummonAbility::GetSpawnLocations() {
+  const FVector Forward =
+      GetAvatarActorFromActorInfo()->GetActorForwardVector();
+  const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+  const float DeltaSpread = SpawnSpread / NumMinions;
+
+  const FVector LeftOfSpread =
+      Forward.RotateAngleAxis(-SpawnSpread / 2.f, FVector::UpVector);
+  TArray<FVector> SpawnLocations;
+  for (int32 i = 0; i < NumMinions; i++) {
+    const FVector Direction =
+        LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+    FVector ChosenSpawnLocation =
+        Location +
+        Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+    FHitResult HitResult;
+    GetWorld()->LineTraceSingleByChannel(
+        HitResult, ChosenSpawnLocation + FVector(0.f, 0.f, 500.f),
+        ChosenSpawnLocation - FVector(0.f, 0.f, 500.f), ECC_Visibility);
+    if (HitResult.bBlockingHit) {
+      ChosenSpawnLocation = HitResult.ImpactPoint;
+    }
+    SpawnLocations.Add(ChosenSpawnLocation);
+  }
+
+  return SpawnLocations;
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/ValueSummonAbility.h
@@ -0,0 +1,33 @@
+// Copyright Philosophical Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/ValueGameplayAbility.h"
+#include "ValueSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UValueSummonAbility : public UValueGameplayAbility
+{
+	GENERATED_BODY()
+  UFUNCTION(BlueprintCallable)
+  TArray<FVector> GetSpawnLocations();
+
+  UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+  int32 NumMinions = 5;
+
+  UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+  TArray<TSubclassOf<APawn>> MinionClasses;
+
+  UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+  float MinSpawnDistance = 50.f;
+
+  UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+  float MaxSpawnDistance = 250.f;
+
+  UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+  float SpawnSpread = 90.f;
+};
