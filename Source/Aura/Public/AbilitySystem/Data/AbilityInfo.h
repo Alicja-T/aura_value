dd1d1e0e059234ee8e79b6e92e8ac2b8af0ede58
Alicja Texler <emunah73@gmail.com>
2024-08-31 21:16:04 -0400

    showing cooldown timer

--- Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
@@ -19,6 +19,9 @@ struct FValueAbilityInfo {
   FGameplayTag InputTag = FGameplayTag();
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+  FGameplayTag CooldownTag = FGameplayTag();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
   TObjectPtr<const UTexture2D> IconTexture = nullptr;
   
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)      
